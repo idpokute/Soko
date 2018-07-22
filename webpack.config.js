a97868b820a66e4c2c08e01e53fc088d62aec86f
Mike Sehui Park <mike.sayhi@gmail.com>
2018-07-21 23:23:58 -0400

    Add Browsersync
    When you run yarn start, browsersync will catch the request and connect by proxy.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path'); // built-in
 const CleanWebpackPlugin = require('clean-webpack-plugin'); // delete folder 
 const HtmlWebpackPlugin = require('html-webpack-plugin');   // adds js load code into html 
-
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -22,7 +22,25 @@ module.exports = {
       title: 'Output Management',
       inject: true,
       template: path.resolve(__dirname, 'index.html'),  // we will use existing file. 
-    })
+    }),
+    new BrowserSyncPlugin(
+      // BrowserSync options
+      {
+        // browse to http://localhost:3000/ during development
+        host: 'localhost',
+        port: 8000,
+        // proxy the Webpack Dev Server endpoint
+        // (which should be serving on http://localhost:3100/)
+        // through BrowserSync
+        proxy: 'http://localhost:3100/'
+      },
+      // plugin options
+      {
+        // prevent BrowserSync from reloading the page
+        // and let Webpack Dev Server take care of this
+        reload: false
+      }
+    )
   ],
   output: {
     filename: '[name].bundle.js',
